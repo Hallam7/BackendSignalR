0edc0608024c1b33a53bbc799233b0d150499d59
Mark Hallam <C16751105@mydit.ie>
2021-04-04 16:13:34 +0100

    Recent
    No real changes made

--- BackendSignalR/Startup.cs
+++ BackendSignalR/Startup.cs
@@ -40,7 +40,8 @@ namespace BackendSignalR
         }
 
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime
+        // Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
